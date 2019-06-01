85e6244eb163edd33d7155e84fdfac249acca4ef
ağl <al@al.al>
2019-06-02 00:11:11 +0300

    Added The Chest Dimension

---
+++ TheChestDimension/ModConfig.cs
@@ -0,0 +1,22 @@
+﻿using StardewModdingAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheChestDimension
+{
+    class ModConfig
+    {
+        public SButton TCDKey { get; set; } = SButton.H;
+
+        public bool ShowEntryMessage { get; set; } = false;
+        public string EntryMessage { get; set; } = "Welcome to the Chest Dimension. Press {TCDkey} or go to the top-left corner to exit.";
+
+        public string CanOnlyEnterFrom { get; set; } //if not empty, implies CanEnterFromMines = false
+        public bool CanEnterFromCave { get; set; } = true;
+        public bool ShowCannotEnterMessage { get; set; } = true;
+        public string CannotEnterMessage { get; set; } = "You cannot enter the Chest Dimension from this location.";
+    }
+}
---
+++ TheChestDimension/ModEntry.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+using StardewValley.Network;
+using xTile;
+
+namespace TheChestDimension
+{
+    public class ModEntry : Mod
+    {
+        private ModConfig config;
+
+        // Player's location and position before the warp
+        GameLocation OldLocation;
+        Vector2 OldPosition;
+
+        public override void Entry(IModHelper helper)
+        {
+            helper.Events.Input.ButtonPressed += Input_ButtonPressed;
+            config = Helper.ReadConfig<ModConfig>();
+        }
+
+        private void Input_ButtonPressed(object sender, ButtonPressedEventArgs e)
+        {
+            if (e.Button == config.TCDKey)
+            {
+                // if already in TCD, go back
+                if (Game1.player.currentLocation.Name == "ChestDimension")
+                {
+                    Game1.warpFarmer(OldLocation.Name, (int)OldPosition.X, (int)OldPosition.Y, false);
+                    return;
+                }
+
+                // shorten string names, for the upcoming monstrosity of an if statement
+                string coef = config.CanOnlyEnterFrom;
+                string curloc = Game1.player.currentLocation.Name;
+
+                if
+                (
+                    // No specific entry location, or if there is any, it's the same as the current location
+                    (coef == null || coef == "" || coef == curloc)
+                    &&
+                    // Player is not in a cave, when CanEnterFromCave is false
+                    !(!config.CanEnterFromCave && curloc.StartsWith("UndergroundMine"))
+                )
+                {
+                    // Remember old locations and warp to TCD
+                    OldLocation = Game1.player.currentLocation;
+                    OldPosition = Game1.player.getTileLocation();
+                    Game1.warpFarmer("ChestDimension", 40, 22, false);
+
+                    // If entry message is enabled, replace variables and show entry message in chat
+                    if (config.ShowEntryMessage)
+                    {
+                        string msg = config.EntryMessage;
+                        msg.Replace("{TCDkey}", config.TCDKey.ToString());
+                        Game1.chatBox.addInfoMessage(msg);
+                    }
+                }
+                else // Cannot enter TCD
+                {
+                    if (config.ShowCannotEnterMessage)
+                    {
+                        Game1.chatBox.addInfoMessage(config.CannotEnterMessage);
+                    }
+                }
+            }
+        }
+    }
+}
