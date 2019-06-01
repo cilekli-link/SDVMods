b7939b689bdc6d86142f40cd14b9b475ed59989b
ağl <al@al.al>
2019-06-02 00:14:17 +0300

    Updated a comment

--- TheChestDimension/ModConfig.cs
+++ TheChestDimension/ModConfig.cs
@@ -14,7 +14,7 @@ namespace TheChestDimension
         public bool ShowEntryMessage { get; set; } = false;
         public string EntryMessage { get; set; } = "Welcome to the Chest Dimension. Press {TCDkey} to exit.";
 
-        public string CanOnlyEnterFrom { get; set; } //if not empty, implies CanEnterFromMines = false
+        public string CanOnlyEnterFrom { get; set; } //if not empty, implies CanEnterFromCave = false
         public bool CanEnterFromCave { get; set; } = true;
         public bool ShowCannotEnterMessage { get; set; } = true;
         public string CannotEnterMessage { get; set; } = "You cannot enter the Chest Dimension from this location.";
