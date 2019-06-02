95934c5222ae248512850eb194b05346c0a2c52b
ağl <al@al.al>
2019-06-03 00:02:48 +0300

    oops, forgot to push (0.0.2 release)

--- TheChestDimension/ModEntry.cs
+++ TheChestDimension/ModEntry.cs
@@ -13,7 +13,7 @@ namespace TheChestDimension
     {
         private ModConfig config;
 
-        // Player's location and position before the warp
+        // player's location and position before the warp
         GameLocation OldLocation;
         Vector2 OldPosition;
 
@@ -28,7 +28,7 @@ namespace TheChestDimension
             if (e.Button == config.TCDKey)
             {
                 // if already in TCD, go back
-                if (Game1.player.currentLocation.Name == "ChestDimension")
+                if (PlayerInTCD)
                 {
                     Game1.warpFarmer(OldLocation.Name, (int)OldPosition.X, (int)OldPosition.Y, false);
                     return;
@@ -40,19 +40,31 @@ namespace TheChestDimension
 
                 if
                 (
-                    // No specific entry location, or if there is any, it's the same as the current location
+                    // no specific entry location, or if there is any, it's the same as the current location
                     (coef == null || coef == "" || coef == curloc)
                     &&
-                    // Player is not in a cave, when CanEnterFromCave is false
+                    // player is not in a cave, when CanEnterFromCave is false
                     !(!config.CanEnterFromCave && curloc.StartsWith("UndergroundMine"))
                 )
                 {
-                    // Remember old locations and warp to TCD
                     OldLocation = Game1.player.currentLocation;
                     OldPosition = Game1.player.getTileLocation();
-                    Game1.warpFarmer("ChestDimension", 40, 22, false);
 
-                    // If entry message is enabled, replace variables and show entry message in chat
+                    // get custom spawn location from save
+                    string spawnX = Helper.Data.ReadSaveData<string>("TCD_spawn_location_X");
+                    string spawnY = Helper.Data.ReadSaveData<string>("TCD_spawn_location_Y");
+
+                    // if custom spawn location is null, warp to default spawn location, else warp to custom spawn location 
+                    if (spawnX == null)
+                    {
+                        Game1.warpFarmer("ChestDimension", 55, 37, false);
+                    }
+                    else
+                    {
+                        Game1.warpFarmer("ChestDimension", int.Parse(spawnX), int.Parse(spawnY), false);
+                    }
+
+                    // if entry message is enabled, replace variables and show entry message in chat
                     if (config.ShowEntryMessage)
                     {
                         string msg = config.EntryMessage;
@@ -60,7 +72,7 @@ namespace TheChestDimension
                         Game1.chatBox.addInfoMessage(msg);
                     }
                 }
-                else // Cannot enter TCD
+                else // cannot enter TCD
                 {
                     if (config.ShowCannotEnterMessage)
                     {
@@ -68,6 +80,27 @@ namespace TheChestDimension
                     }
                 }
             }
+            else if (e.Button == config.SetSpawnKey)
+            {
+                if (PlayerInTCD)
+                {
+                    // save current location as spawn location
+
+                    string xPos = Game1.player.getTileX().ToString();
+                    string yPos = Game1.player.getTileY().ToString();
+
+                    Helper.Data.WriteSaveData("TCD_spawn_location_X", xPos);
+                    Helper.Data.WriteSaveData("TCD_spawn_location_Y", yPos);
+
+                    Game1.chatBox.addInfoMessage("TCD spawn location set to " + xPos + "," + yPos + ".");
+                }
+                else
+                {
+                    Game1.chatBox.addErrorMessage("You have to be inside TCD to set its spawn location.");
+                }
+            }
         }
+
+        private bool PlayerInTCD => Game1.player.currentLocation.Name == "ChestDimension";
     }
 }
